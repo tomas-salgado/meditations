b4ac9effbdb57eb211ee9fb1f925986266109e03
Tomas Salgado <91388965+tomas-salgado@users.noreply.github.com>
2024-11-27 15:31:48 -0600

    add tsconfig.json files and turbo.json and nextjs init files

---
+++ frontend/next.config.mjs
@@ -0,0 +1,14 @@
+export default {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3001/api/:path*'
+      }
+    ]
+  },
+  output: 'standalone',
+  experimental: {
+    appDir: true
+  }
+}
