494dc28e0eb977e742f4a349a5182b7f343815a4
Tomas Salgado <91388965+tomas-salgado@users.noreply.github.com>
2024-11-29 09:19:50 -0600

    code cleanup

--- backend/server.ts
+++ backend/server.ts
@@ -9,7 +9,6 @@ import { dirname } from 'path';
 import cors from 'cors';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3001;
